c282bf3d7107466a01636bcbfff036b9ad0ae66d
François Njeneza <njenezamed@gmail.com>
2013-10-26 15:45:11 +0200

    Connexion à la base de données

---
+++ model/model.inc.php
@@ -0,0 +1,20 @@
+<?php
+define('HOSTNAME', "localhost");
+define('DBNAME' , "mp3_db");
+define('USERNAME', "root");
+define('PASSWORD', "123456");
+
+/*
+ * Connexion à la base de données
+*/
+function openConnection(){
+	$db=new mysqli(HOSTNAME, USERNAME, PASSWORD, DBNAME);
+	if($db->error){
+		die("Erreur de connexion".$db->error);
+	}
+	return $db;
+}
+
+function closeConnection($bdd){
+	$bdd->close;
+}
