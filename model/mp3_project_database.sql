8e0ff82be1ff247d81251e35ec9cbc780c136d06
François Njeneza <njenezamed@gmail.com>
2013-11-01 11:23:47 +0100

    ajout de clés étrangères

--- model/mp3_project_database.sql
+++ model/mp3_project_database.sql
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS `artiste` (
   `nom` varchar(255) NOT NULL,
   `url` varchar(255) NOT NULL,
   PRIMARY KEY (`nom`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -41,57 +41,16 @@ CREATE TABLE IF NOT EXISTS `artiste` (
 CREATE TABLE IF NOT EXISTS `chanson` (
   `id` int(11) NOT NULL,
   `titre` varchar(255) NOT NULL,
-  `artiste` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `artiste` varchar(255) NOT NULL,
   `annee` int(11) NOT NULL,
   `album` varchar(255) NOT NULL,
   `genre` varchar(255) NOT NULL,
   `date_d_ajout` date NOT NULL,
   `url_image` varchar(255) NOT NULL,
   `url_chanson` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `commentaire`
---
-
-CREATE TABLE IF NOT EXISTS `commentaire` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `pseudo_commentateur` varchar(255) NOT NULL,
-  `titre_commentaire` varchar(255) NOT NULL,
-  `intitule` text NOT NULL,
-  `date_commentaire` date NOT NULL,
-  `id_chanson` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `playlist`
---
-
-CREATE TABLE IF NOT EXISTS `playlist` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nom_playlist` varchar(255) NOT NULL,
-  `pseudo` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `playlist_chanson`
---
-
-CREATE TABLE IF NOT EXISTS `playlist_chanson` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_playlist` int(11) NOT NULL,
-  `id_chanson` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  PRIMARY KEY (`id`),
+  FOREIGN KEY(`artiste`) REFERENCES artiste(`nom`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -109,7 +68,54 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
   `email` varchar(255) NOT NULL,
   `url_photo` varchar(255) NOT NULL,
   PRIMARY KEY (`pseudo`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commentaire`
+--
+
+CREATE TABLE IF NOT EXISTS `commentaire` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pseudo_commentateur` varchar(255) NOT NULL,
+  `titre_commentaire` varchar(255) NOT NULL,
+  `intitule` text NOT NULL,
+  `date_commentaire` date NOT NULL,
+  `id_chanson` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY(`pseudo_commentateur`) REFERENCES utilisateur(`pseudo`),
+  FOREIGN KEY(`id_chanson`) REFERENCES chanson(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `playlist`
+--
+
+CREATE TABLE IF NOT EXISTS `playlist` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom_playlist` varchar(255) NOT NULL,
+  `pseudo` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY(`pseudo`) REFERENCES utilisateur(`pseudo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `playlist_chanson`
+--
+
+CREATE TABLE IF NOT EXISTS `playlist_chanson` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_playlist` int(11) NOT NULL,
+  `id_chanson` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY(`id_chanson`) REFERENCES chanson(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
